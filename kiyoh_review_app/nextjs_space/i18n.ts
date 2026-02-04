347e10eb641f1930b895b47b660a348680e92d66
Xeno9948 <stathisxeno@icloud.com>
2026-02-04 12:58:50 +0200

    Feat: Add i18n configuration and message files

---
+++ kiyoh_review_app/nextjs_space/i18n.ts
@@ -0,0 +1,15 @@
+import { getRequestConfig } from 'next-intl/server';
+import { cookies } from 'next/headers';
+
+export const locales = ['en', 'nl', 'de', 'fr', 'es'];
+export const defaultLocale = 'en';
+
+export default getRequestConfig(async () => {
+    const cookieStore = cookies();
+    const locale = cookieStore.get('NEXT_LOCALE')?.value || defaultLocale;
+
+    return {
+        locale,
+        messages: (await import(`./messages/${locale}.json`)).default
+    };
+});
