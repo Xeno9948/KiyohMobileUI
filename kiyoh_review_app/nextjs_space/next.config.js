f0e280e1fa9959019d77746f79ac1fa47d89fa44
Xeno9948 <stathisxeno@icloud.com>
2026-02-04 13:44:22 +0200

    Fix: Build error & Add SystemSettings schema

--- kiyoh_review_app/nextjs_space/next.config.js
+++ kiyoh_review_app/nextjs_space/next.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const withNextIntl = require('next-intl/plugin')();
+const withNextIntl = require('next-intl/plugin')('./i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
